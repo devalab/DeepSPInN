cd80dd03929087330a58d4cc90ae173ad11b75ff
Sriram <sriramdvt@gmail.com>
2024-01-09 11:15:24 +0530

    Added sbatch script for testing

---
+++ miscellaneous/results_deepspinn/sbatch_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --mem-per-cpu=3000
+#SBATCH -A research
+#SBATCH -n 39
+#SBATCH --gres=gpu:4
+#SBATCH -t 4-00:00:00
+#SBATCH --mail-type=NONE
+#SBATCH --job-name="Upto 1k"
+#SBATCH --output=ir_nmr_file.txt
+#SBATCH --exclude=gnode051,gnode052
+
+ulimit -n 40960
+
+source "/home2/$USER/miniconda3/etc/profile.d/conda.sh"
+source "/home2/$USER/miniconda3/etc/profile.d/mamba.sh"
+
+rm -r "/scratch/$USER/deepspinn/"
+mkdir -p "/scratch/$USER"
+rm -r "/scratch/$USER/conda_pkgs_dirs"
+mkdir -p "/scratch/$USER/conda_pkgs_dirs"
+export CONDA_PKGS_DIRS="/scratch/$USER/conda_pkgs_dirs"
+
+mamba create --prefix "/scratch/$USER/deepspinn" python=3.9.6 --yes
+mamba activate "/scratch/$USER/deepspinn"
+module load u18/cuda/10.2
+
+pip install torch==1.9.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+mamba install -c conda-forge ipdb --yes
+mamba install -c dglteam dgl-cuda10.2 --yes
+mamba install -c conda-forge rdkit --only-deps --yes
+pip install protobuf==3.20.*
+pip install rdkit-pypi
+pip install tqdm ray chemprop==1.3.0
+
+cd "/home2/$USER/1k/1k/test"
+ray start --head --num-cpus=39 --num-gpus=4 --object-store-memory 50000000000
+python parallel_agent.py -s 0 -e 2000
+
